d330a5703de2513e8dce44a2d3f5f86a735094df
kiran04-code <kr551344@gmail.com>
2026-01-18 17:54:32 +0530

    day05

--- Khanaval_ui/src/components/layout/Navbar.tsx
+++ Khanaval_ui/src/components/layout/Navbar.tsx
@@ -53,7 +53,7 @@ export function Navbar() {
       <div className="container mx-auto px-6">
         <div className="flex items-center justify-between">
           
-          <Link to="/" className="md:w-[170px] w-[200px] md:h-fit">
+          <Link to="/" className="md:w-[170px] w-[15px] md:h-fit">
             <img src="/logo.png" alt="logo" className="w-full h-auto" />
           </Link>
 

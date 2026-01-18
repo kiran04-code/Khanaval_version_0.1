64326ebcd6a0a782da243f289a4d3483afa6f8a6
kiran04-code <kr551344@gmail.com>
2026-01-18 18:01:23 +0530

    day05

--- Khanaval_ui/src/pages/Index.tsx
+++ Khanaval_ui/src/pages/Index.tsx
@@ -159,7 +159,7 @@ const {userlat,userlng} = useStateContex()
           <div className="grid lg:grid-cols-2 gap-16 items-center">
 
             {/* LEFT CONTENT */}
-            <div className="text-center py-[45px] lg:text-left animate-slide-up z-10">
+            <div className="text-center py-[25px] lg:text-left animate-slide-up z-10">
               <Badge variant="soft" className="mb-6 px-4 py-1.5 text-sm uppercase tracking-wide border-orange-100">
                 🍛 India’s Trusted Mess Discovery
               </Badge>

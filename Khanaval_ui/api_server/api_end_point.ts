644a0eb7f82af4e7a5c7abc456ed58359d1b72e9
kiran04-code <kr551344@gmail.com>
2026-01-27 11:00:50 +0530

    done

--- Khanaval_ui/api_server/api_end_point.ts
+++ Khanaval_ui/api_server/api_end_point.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request"
 const isClient = typeof window !== "undefined";
 const backend_url  = import.meta.env.VITE_BACKEND_API;
-console.log(backend_url)
+
 export const graphqlClient = new GraphQLClient(`${backend_url}/graphql`,{
     headers:()=>({
         Authorization: isClient  ? `Bearer ${window.localStorage.getItem("_user_Token__")}` :""

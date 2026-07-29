e113b363f939a88bbad6a89447812d28d10234b0
kiran04-code <kr551344@gmail.com>
2026-07-30 01:25:10 +0530

    some_imp:redis_Update

--- Khanaval_server/dist/config/redis.js
+++ Khanaval_server/dist/config/redis.js
@@ -1,5 +1,5 @@
 import { Redis } from "ioredis";
-export const redisclient = new Redis("redis://default:gQAAAAAAAqAYAAIgcDJkZjM1NDJhMjBhNDg0Y2U3YTJkZThlZDdlOGMzMWUxZQ@talented-anchovy-172056.upstash.io:6379", {
+export const redisclient = new Redis("rediss://default:gQAAAAAAAqAYAAIgcDJkZjM1NDJhMjBhNDg0Y2U3YTJkZThlZDdlOGMzMWUxZQ@talented-anchovy-172056.upstash.io:6379", {
     enableReadyCheck: false,
     maxRetriesPerRequest: null,
 });
--- Khanaval_server/src/config/redis.ts
+++ Khanaval_server/src/config/redis.ts
@@ -1,7 +1,7 @@
 import { Redis } from "ioredis";
 
 export const redisclient = new Redis(
-  "redis://default:gQAAAAAAAqAYAAIgcDJkZjM1NDJhMjBhNDg0Y2U3YTJkZThlZDdlOGMzMWUxZQ@talented-anchovy-172056.upstash.io:6379",
+  "rediss://default:gQAAAAAAAqAYAAIgcDJkZjM1NDJhMjBhNDg0Y2U3YTJkZThlZDdlOGMzMWUxZQ@talented-anchovy-172056.upstash.io:6379",
   {
     enableReadyCheck: false,
     maxRetriesPerRequest: null,
